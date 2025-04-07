9091e3e38b936378cd1a0d9a16633a1b053d79ec
LaimisMilas <laimis.milasius@gmail.com>
2025-04-08 00:02:27 +0300

    TickerService.js implementation

--- login/index.js
+++ login/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import {createElementByTagName, injectElement} from "html-evaluate-utils/Utils";
 import App from "./App";
 
-const rootElementId = "labas_as_krabas_root";
+const rootElementId = "login_page_root";
 injectElement('//body', createElementByTagName("div", rootElementId));
 const rootElement = document.getElementById(rootElementId);
 
